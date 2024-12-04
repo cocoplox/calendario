26721abbc795eb54443511a55526886f1ce9baeb
cocoplox <ivanvegasantaella5@gmail.com>
2024-12-04 15:53:18 +0100

    Test test

--- src/test/java/usr/ivan/AppTest.java
+++ src/test/java/usr/ivan/AppTest.java
@@ -1,5 +1,6 @@
 package usr.ivan;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -16,5 +17,13 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+
+    }
+    @Test
+    public void HelloWorldTests(){
+        String esperado = "Hola Mundo";
+        App app = new App();
+        String resultado = app.Hola();
+        assertEquals(esperado,resultado);
     }
 }
